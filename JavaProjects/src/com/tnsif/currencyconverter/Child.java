9e3405b01a311587c694383d4a7720a092ea3467
Shaik Azhar Sohail <155532610+SohailShaikAzhar@users.noreply.github.com>
2024-07-15 20:24:08 +0530

    Starting on Java GUI by JFrames

---
+++ JavaProjects/src/com/tnsif/currencyconverter/Child.java
@@ -0,0 +1,21 @@
+package com.tnsif.currencyconverter;
+
+import java.awt.Color;    //to import the colors
+import javax.swing.ImageIcon;    //to change image icon
+import javax.swing.JFrame;    //importing JFrame
+
+public class Child extends JFrame
+{
+    Child()
+    {
+	    this.setTitle("Currency Exchange");    //Sets title of the this
+	    this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);    //exit the application
+	    this.setResizable(false);    //prevent this from resize
+	    this.setSize(420, 420);    //sets x and y dimension of the this
+	    this.setVisible(true);    //make this to visible
+	    
+	    ImageIcon image = new ImageIcon("currencyexchange.png");    //create an image icon
+	    this.setIconImage(image.getImage());    //change icon of the this
+	    this.getContentPane().setBackground(new Color(111, 200, 232));    //change the color of the this
+    }
+}
